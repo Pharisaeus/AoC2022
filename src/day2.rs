59d62b3e65b1faa8106bcacd8d257e05e3cd51ed
Pharisaeus <Pharisaeus@users.noreply.github.com>
2022-12-04 16:40:19 +0100

    add enums to day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,75 +1,92 @@
 use std::fs;
 use itertools::Itertools;
+use crate::day2::RPS::{Paper, Rock, Scissors};
 
-fn get_move_points(choice: &str) -> i32 {
-    match choice {
-        "A" => 1,
-        "B" => 2,
-        "C" => 3,
-        _ => panic!()
+#[derive(Copy, Clone, PartialEq)]
+enum RPS {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl RPS {
+    pub fn new(choice: &str) -> Self {
+        match choice {
+            "A" | "X" => Rock,
+            "B" | "Y" => Paper,
+            "C" | "Z" => Scissors,
+            _ => panic!()
+        }
     }
 }
 
-fn get_round_points(first: &str, second: &str) -> i32 {
-    return if get_winning_move(first) == second {
-        6
-    } else if first == second {
-        3
+#[derive(Copy, Clone, PartialEq)]
+enum Result {
+    Win = 6,
+    Draw = 3,
+    Lose = 0,
+}
+
+impl Result {
+    pub fn new(choice: &str) -> Self {
+        match choice {
+            "X" => Result::Lose,
+            "Y" => Result::Draw,
+            "Z" => Result::Win,
+            _ => panic!()
+        }
+    }
+}
+
+fn get_round_result(opponent: &RPS, me: &RPS) -> Result {
+    return if get_winning_move(opponent) == *me {
+        Result::Win
+    } else if opponent == me {
+        Result::Draw
     } else {
-        0
+        Result::Lose
     };
 }
 
-fn map_move(choice: &str) -> &str {
+
+fn get_round_score(opponent: RPS, me: RPS) -> i32 {
+    (me as i32) + (get_round_result(&opponent, &me) as i32)
+}
+
+fn get_winning_move(choice: &RPS) -> RPS {
     match choice {
-        "X" => "A",
-        "Y" => "B",
-        "Z" => "C",
-        _ => panic!()
+        Rock => Paper,
+        Paper => Scissors,
+        Scissors => Rock,
     }
 }
 
-fn get_game_score(rounds: Vec<(&str, String)>) -> i32 {
-    rounds.iter()
-        .map(|(opponent, me)| get_move_points(me) + get_round_points(opponent, me))
-        .sum()
-}
-
-fn get_winning_move(choice: &str) -> &str {
-    match choice {
-        "A" => "B",
-        "B" => "C",
-        "C" => "A",
-        _ => panic!()
-    }
-}
-
-fn get_losing_move(choice: &str) -> &str {
-    get_winning_move(get_winning_move(choice))
+fn get_losing_move(choice: &RPS) -> RPS {
+    get_winning_move(&get_winning_move(choice))
 }
 
 
-fn get_move_to_play(choice: &str, result_type: &str) -> String {
+fn get_move_to_play(opponent: &RPS, result_type: &Result) -> RPS {
     match result_type {
-        "X" => get_losing_move(choice),
-        "Y" => choice,
-        "Z" => get_winning_move(choice),
-        &_ => panic!()
-    }.to_string()
+        Result::Win => get_winning_move(opponent),
+        Result::Draw => *opponent,
+        Result::Lose => get_losing_move(opponent),
+    }
 }
 
 fn part1(rounds: &Vec<(&str, &str)>) -> i32 {
-    let played_rounds = rounds.iter()
-        .map(|&(opponent, me)| (opponent, map_move(me).to_string()))
-        .collect_vec();
-    get_game_score(played_rounds)
+    rounds.iter()
+        .map(|&(opponent, me)| (RPS::new(opponent), RPS::new(me)))
+        .map(|(opponent, me)| get_round_score(opponent, me))
+        .sum()
 }
 
 fn part2(rounds: &Vec<(&str, &str)>) -> i32 {
-    let played_rounds = rounds.iter()
-        .map(|&(opponent, result_type)| (opponent, get_move_to_play(opponent, result_type)))
-        .collect_vec();
-    get_game_score(played_rounds)
+    rounds.iter()
+        .map(|&(opponent, result_type)| (RPS::new(opponent), Result::new(result_type)))
+        .map(|(opponent, result)| (opponent, get_move_to_play(&opponent, &result)))
+        .map(|(opponent, me)| get_round_score(opponent, me))
+        .sum()
 }
 
 pub(crate) fn solve() {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate core;
-
 mod day1;
 mod day2;
 mod day3;
