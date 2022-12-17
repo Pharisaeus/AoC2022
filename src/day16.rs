7599aa7a7d3a179acc008e64217a074410b9695d
Pharisaeus <Pharisaeus@users.noreply.github.com>
2022-12-17 01:27:30 +0100

    day16 optimize a bit

--- src/day16.rs
+++ src/day16.rs
@@ -47,71 +47,72 @@ fn parse_graph(data: &str) -> (HashMap<String, HashSet<String>>, HashMap<String,
     let mut flow = HashMap::new();
     for line in data.split("\n") {
         let (name, rate, neighbours) = parse_line(line);
+        if rate > 0 {
+            flow.insert(name.clone(), rate);
+        }
         graph.insert(name.clone(), neighbours);
-        flow.insert(name.clone(), rate);
     }
     (graph, flow)
 }
 
-fn find_path(flows: &HashMap<String, i32>, distances: &HashMap<(&String, &String), i32>, steps_left: i32, visited: &HashSet<&String>, current: &String, score: i32, rate: i32) -> i32 {
+fn find_path(nodes: &HashSet<&String>, flows: &HashMap<String, i32>, distances: &HashMap<(&String, &String), i32>, steps_left: i32, visited: &HashSet<&String>, current: &String, score: i32, rate: i32) -> i32 {
     return if steps_left <= 0 {
         score
     } else {
-        let potential_targets = flows.keys()
-            .into_iter()
-            .filter(|&v| v != current && !visited.contains(v) && *flows.get(v).unwrap() > 0 && *distances.get(&(current, v)).unwrap() < steps_left)
-            .collect_vec();
         let mut best = score + rate * steps_left;
-        for t in potential_targets {
+        let mut new_visited = visited.clone();
+        new_visited.insert(current);
+        let potential_targets = nodes.difference(&new_visited);
+        for &t in potential_targets {
             let d = distances.get(&(current, t)).unwrap() + 1;
-            let r = flows.get(t).unwrap();
-            let new_visited: HashSet<&String> = visited.union(&HashSet::from([current]))
-                .map(|x| *x)
-                .collect();
-            let s = find_path(flows, distances, steps_left - d, &new_visited, t, score + rate * d, rate + r);
-            if s > best {
-                best = s;
+            if d < steps_left {
+                let r = flows.get(t).unwrap();
+                let s = find_path(nodes, flows, distances, steps_left - d, &new_visited, t, score + rate * d, rate + r);
+                if s > best {
+                    best = s;
+                }
             }
         }
         best
     };
 }
 
-fn find_dual_path(flows: &HashMap<String, i32>, distances: &HashMap<(&String, &String), i32>,
+fn find_dual_path(nodes: &HashSet<&String>, flows: &HashMap<String, i32>, distances: &HashMap<(&String, &String), i32>,
                   steps_left_me: i32, steps_left_el: i32, visited: &HashSet<&String>,
                   current_me: &String, current_el: &String,
                   score_me: i32, score_el: i32, rate_me: i32, rate_el: i32) -> i32 {
-    let potential_targets_me = flows.keys()
-        .into_iter()
-        .filter(|&v| v != current_me && v != current_el && !visited.contains(v) && *flows.get(v).unwrap() > 0 && *distances.get(&(current_me, v)).unwrap() < steps_left_me)
+    let mut new_visited = visited.clone();
+    new_visited.insert(current_me);
+    new_visited.insert(current_el);
+    let potential_targets = nodes.difference(&new_visited).collect_vec();
+    let potential_targets_me = potential_targets.iter()
+        .filter(|&&&v| *distances.get(&(current_me, v)).unwrap() < steps_left_me)
         .collect_vec();
-    let potential_targets_el = flows.keys()
-        .into_iter()
-        .filter(|&v| v != current_el && v != current_me && !visited.contains(v) && *flows.get(v).unwrap() > 0 && *distances.get(&(current_el, v)).unwrap() < steps_left_el)
+    let potential_targets_el = potential_targets.iter()
+        .filter(|&&&v| *distances.get(&(current_el, v)).unwrap() < steps_left_el)
         .collect_vec();
-    let new_visited: HashSet<&String> = visited.union(&HashSet::from([current_el, current_me]))
-        .map(|x| *x)
-        .collect();
-    return if potential_targets_me.len() == 0 && potential_targets_el.len() == 0 {
-        score_me + score_el + steps_left_el * rate_el + steps_left_me * rate_me
-    } else if potential_targets_me.len() == 0 && potential_targets_el.len() > 0 {
-        find_path(flows, distances, steps_left_el, &new_visited, current_el, score_me + steps_left_me * rate_me + score_el, rate_el)
+    return if potential_targets_me.len() == 0 && potential_targets_el.len() > 0 {
+        find_path(nodes, flows, distances, steps_left_el, &new_visited, current_el, score_me + steps_left_me * rate_me + score_el, rate_el)
     } else if potential_targets_el.len() == 0 && potential_targets_me.len() > 0 {
-        find_path(flows, distances, steps_left_me, &new_visited, current_me, score_el + steps_left_el * rate_el + score_me, rate_me)
+        find_path(nodes, flows, distances, steps_left_me, &new_visited, current_me, score_el + steps_left_el * rate_el + score_me, rate_me)
     } else {
         let mut best = score_me + score_el + rate_me * steps_left_me + rate_el * steps_left_el;
-        for my_target in potential_targets_me {
+        for &&my_target in potential_targets_me {
             let d_me = distances.get(&(current_me, my_target)).unwrap() + 1;
-            let r_me = flows.get(my_target).unwrap();
-            for &el_target in &potential_targets_el {
-                if my_target != el_target {
-                    let d_el = distances.get(&(current_el, el_target)).unwrap() + 1;
-                    let r_el = flows.get(el_target).unwrap();
-                    let s = find_dual_path(flows, distances, steps_left_me - d_me, steps_left_el - d_el,
-                                           &new_visited, my_target,
-                                           el_target, score_me + rate_me * d_me, score_el + rate_el * d_el, rate_me + r_me, rate_el + r_el);
-                    if s > best {
-                        best = s;
+            if d_me < steps_left_me {
+                let r_me = flows.get(my_target).unwrap();
+                for &&&el_target in &potential_targets_el {
+                    if my_target != el_target {
+                        let d_el = distances.get(&(current_el, el_target)).unwrap() + 1;
+                        if d_el < steps_left_el {
+                            let r_el = flows.get(el_target).unwrap();
+                            let s = find_dual_path(nodes, flows, distances, steps_left_me - d_me, steps_left_el - d_el,
+                                                   &new_visited, my_target,
+                                                   el_target, score_me + rate_me * d_me, score_el + rate_el * d_el, rate_me + r_me, rate_el + r_el);
+                            if s > best {
+                                best = s;
+                            }
+                        }
                     }
                 }
             }
@@ -121,11 +122,13 @@ fn find_dual_path(flows: &HashMap<String, i32>, distances: &HashMap<(&String, &S
 }
 
 fn part2(flows: &HashMap<String, i32>, distances: &HashMap<(&String, &String), i32>) -> i32 {
-    find_dual_path(flows, distances, 26, 26, &HashSet::new(), &"AA".to_string(), &"AA".to_string(), 0, 0, 0, 0)
+    let nodes: HashSet<&String> = flows.keys().collect();
+    find_dual_path(&nodes, flows, distances, 26, 26, &HashSet::new(), &"AA".to_string(), &"AA".to_string(), 0, 0, 0, 0)
 }
 
 fn part1(flows: &HashMap<String, i32>, distances: &HashMap<(&String, &String), i32>) -> i32 {
-    find_path(flows, distances, 30, &HashSet::new(), &"AA".to_string(), 0, 0)
+    let nodes: HashSet<&String> = flows.keys().collect();
+    find_path(&nodes, flows, distances, 30, &HashSet::new(), &"AA".to_string(), 0, 0)
 }
 
 pub(crate) fn solve() {
